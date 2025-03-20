4ebfff2f4b0204e5351340697ee3fec8e167dca0
Bruno Francisco <64901068+brunofranciscojs@users.noreply.github.com>
2025-03-20 00:28:08 -0300

    Update ComicBook.jsx

--- src/components/ComicBook.jsx
+++ src/components/ComicBook.jsx
@@ -31,18 +31,20 @@ export default function ComicBook({file, setComic, openComicFromDrive}){
 
     return(
          
-        <div className="flex justify-between flex-col lg:flex-row gap-8 p-12 [background:var(--bg)_#000d] bg-blend-multiply !bg-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[90dvh] max-w-[80%] w-full rounded-2xl z-[999]" style={{"--bg":`url(/../assets/${bg}.jpg)`}}>
+        <div className="flex justify-between flex-col lg:flex-row gap-8 p-12 
+             after:content-[''] after:absolute after:left-0 after:top-0 after:w-full after:h-full after:[background:var(--bg)] after:bg-[length:200px] after:brightness-40 after:z-0
+            after:!bg-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[90dvh] max-w-[80%] w-full rounded-2xl z-[999]" style={{"--bg":`url(/../assets/${bg}.jpg)`}}>
             <button className="bg-black text-white rounded-full w-5 h-5 absolute top-4 right-4 z-10" onClick={() => setComic(false)}>X</button>
-            <div>
+            <div className="z-10 relative">
                 <h3 className="text-3xl"><span className="font-['impact'] text-[#f4ed24] text-3xl">Comic:</span> {info.title}</h3>
                 <h3 className="text-3xl"><span className="font-['impact'] text-[#f4ed24] text-3xl">Edition:</span> {info.issue}</h3>
                 <h3 className="text-3xl"><span className="font-['impact'] text-[#f4ed24] text-3xl">Year:</span> {info.year}</h3> <br />
-                <span className="text-gray-200 text-2xl block my-4 max-w-[85%]">{info.description}</span>
+                <span className="text-gray-200 lg:text-2xl text-base block my-4 max-w-[85%]">{info.description}</span>
                 <button onClick={() => openComicFromDrive(file.id, file.fileName)} className="mt-2 bg-[#f4ed24] hover:bg-[#00bcf0] text-[#303539] py-2 px-4 rounded transition z-20">
                     Read
                 </button>
             </div>
-            <img src={`/assets/${bg}.jpg`} className="object-contain w-auto h-[90%] self-center rounded-lg"/>
+            <img src={`/assets/${bg}.jpg`} className="object-contain w-auto h-[90%] self-center z-10 relative rounded-lg"/>
         </div>
         
     )
